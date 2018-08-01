8196e673700990d478f57ef2d3ea7d89ed85f5ae
Food <cpud1258@gmail.com>
2018-08-01 15:23:07 +0700

    them Log

---
+++ DAL/Model/Log/ErrorLogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Model
+{
+   public class ErrorLogs
+    {
+        [Key]
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public string TableName { get; set; }
+        public string ErrorLog { get; set; } = string.Empty;
+        public string ModuleName { get; set; }
+        public string FunctionName { get; set; }
+    }
+}
---
+++ DAL/Model/Log/ImageUploadLog.cs
@@ -0,0 +1,21 @@
+﻿using Common.Enum.Log;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Model.Log
+{
+   public class ImageUploadLog
+    {
+        [Key]
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public virtual System_User CreatedUser { get; set; }
+        public ImageUploadEnum ImageUploadEnum { get; set; } = ImageUploadEnum.Local;
+        public string Erros { get; set; }
+        public string IsSuccess { get; set; }
+        public string IdImage { get; set; }
+
+    }
+}
