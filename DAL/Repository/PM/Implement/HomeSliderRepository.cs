079591369402d520e3ac4f0580bae9f5e2740e1c
Food <cpud1258@gmail.com>
2018-08-01 19:06:21 +0700

    add default connect string

--- DAL/Repository/PM/Implement/HomeSliderRepository.cs
+++ DAL/Repository/PM/Implement/HomeSliderRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using DAL.DataContext;
+using DAL.Model.PM;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DAL.Repository.PM.Implement
 {
-    public class HomeSliderRepository
+    public class HomeSliderRepository : GenericRepository<HomeSlider, string>, IHomeSliderRepository
     {
+        public HomeSliderRepository(ShopContext context) : base(context)
+        {
+        }
     }
 }
