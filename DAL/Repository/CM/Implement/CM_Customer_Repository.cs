618bd05f42d54f7fc02436325bff8ba16e3ebcbf
Food <cpud1258@gmail.com>
2018-08-01 16:06:20 +0700

    Hoan thanh repository pattern

---
+++ DAL/Repository/CM/Implement/CM_Customer_Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repository.CM.Implement
+{
+    class CM_Customer_Repository
+    {
+    }
+}
---
+++ DAL/Repository/CRM/Implement/CRM_FeedBack_Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repository.CRM.Implement
+{
+    class CRM_FeedBack_Repository
+    {
+    }
+}
