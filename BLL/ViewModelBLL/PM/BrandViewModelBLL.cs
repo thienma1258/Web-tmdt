aac87ec7ddf0ed6057f8fdd04e112e5dcb0e4c88
Food <cpud1258@gmail.com>
2018-08-02 17:51:26 +0700

    Xoa common

--- BLL/ViewModelBLL/PM/BrandViewModelBLL.cs
+++ BLL/ViewModelBLL/PM/BrandViewModelBLL.cs
@@ -1,4 +1,4 @@
-﻿using Common.ViewModel.PM;
+﻿
 using DAL;
 using DAL.Model;
 using DAL.Model.PM;
@@ -10,117 +10,117 @@ using System.Threading.Tasks;
 
 namespace BLL.ViewModelBLL
 {
-    public class BrandViewModelBLL
-    {
-        private readonly IUnitOfWork unitOfWork;
-        public BrandViewModelBLL(IUnitOfWork UnitOfWork)
-        {
-            unitOfWork = UnitOfWork;
-        }
-        public async Task<bool> Delete(BrandViewModel objBrandViewModel,string DeletedUser)
-        {
-            try
-            {
-                return false;
-                //Brand brand = new Brand
-                //{
-                //    ID = objBrandViewModel,
-                //    DefaultImage = objBrandViewModel.defaultImages,
-                //    Description = objBrandViewModel.defaultImages
-                //};
-                //this.unitOfWork.BrandRepository.Insert(brand);
-                //await this.unitOfWork.SaveChangeAsync();
-                //return true;
-            }
-            catch (Exception objEx)
-            {
-                ErrorLogs errorLogs = new ErrorLogs
-                {
-                    ErrorLog = objEx.ToString(),
-                    FunctionName = MethodBase.GetCurrentMethod().ToString(),
-                    ModuleName = "PM->Brand"
+    //public class BrandViewModelBLL
+    //{
+    //    private readonly IUnitOfWork unitOfWork;
+    //    public BrandViewModelBLL(IUnitOfWork UnitOfWork)
+    //    {
+    //        unitOfWork = UnitOfWork;
+    //    }
+    //    public async Task<bool> Delete(BrandViewModel objBrandViewModel,string DeletedUser)
+    //    {
+    //        try
+    //        {
+    //            return false;
+    //            //Brand brand = new Brand
+    //            //{
+    //            //    ID = objBrandViewModel,
+    //            //    DefaultImage = objBrandViewModel.defaultImages,
+    //            //    Description = objBrandViewModel.defaultImages
+    //            //};
+    //            //this.unitOfWork.BrandRepository.Insert(brand);
+    //            //await this.unitOfWork.SaveChangeAsync();
+    //            //return true;
+    //        }
+    //        catch (Exception objEx)
+    //        {
+    //            ErrorLogs errorLogs = new ErrorLogs
+    //            {
+    //                ErrorLog = objEx.ToString(),
+    //                FunctionName = MethodBase.GetCurrentMethod().ToString(),
+    //                ModuleName = "PM->Brand"
 
-                };
-                this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
-                await this.unitOfWork.SaveChangeAsync();
-                return false;
-            }
-        }
-        public async Task<bool> Add(BrandViewModel objBrandViewModel,string CreatedUser="adminstrator")
-        {
-            try
-            {
-                Brand brand = new Brand
-                {
-                    Name = objBrandViewModel.nameBrand,
-                    DefaultImage = objBrandViewModel.defaultImages,
-                    Description = objBrandViewModel.defaultImages,
-                    CreatedUser=CreatedUser
-                };
-                this.unitOfWork.BrandRepository.Insert(brand);
-                await  this.unitOfWork.SaveChangeAsync();
-                return true;
-            }
-            catch(Exception objEx)
-            {
-                ErrorLogs errorLogs = new ErrorLogs
-                {
-                    ErrorLog = objEx.ToString(),
-                    FunctionName = MethodBase.GetCurrentMethod().ToString(),
-                    ModuleName = "PM->Brand"
+    //            };
+    //            this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
+    //            await this.unitOfWork.SaveChangeAsync();
+    //            return false;
+    //        }
+    //    }
+    //    public async Task<bool> Add(BrandViewModel objBrandViewModel,string CreatedUser="adminstrator")
+    //    {
+    //        try
+    //        {
+    //            Brand brand = new Brand
+    //            {
+    //                Name = objBrandViewModel.nameBrand,
+    //                DefaultImage = objBrandViewModel.defaultImages,
+    //                Description = objBrandViewModel.defaultImages,
+    //                CreatedUser=CreatedUser
+    //            };
+    //            this.unitOfWork.BrandRepository.Insert(brand);
+    //            await  this.unitOfWork.SaveChangeAsync();
+    //            return true;
+    //        }
+    //        catch(Exception objEx)
+    //        {
+    //            ErrorLogs errorLogs = new ErrorLogs
+    //            {
+    //                ErrorLog = objEx.ToString(),
+    //                FunctionName = MethodBase.GetCurrentMethod().ToString(),
+    //                ModuleName = "PM->Brand"
 
-                };
-                this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
-                await this.unitOfWork.SaveChangeAsync();
-                return false;
-            }
-        }
+    //            };
+    //            this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
+    //            await this.unitOfWork.SaveChangeAsync();
+    //            return false;
+    //        }
+    //    }
 
-        public  List<BrandViewModel> Get(int intNumber,int intSkippage)
-        {
+    //    public  List<BrandViewModel> Get(int intNumber,int intSkippage)
+    //    {
 
-            var listBrand=this.unitOfWork.BrandRepository.Get(filter:p=>p.isDeleted==false,number: intNumber,skippage:intSkippage);
-            var listBrandViewModel = new List<BrandViewModel>();
-            foreach (var Brand in listBrand)
-            {
-                BrandViewModel objBrandViewModel = new BrandViewModel
-                {
-                    defaultImages = Brand.DefaultImage,
-                    nameBrand = Brand.Name,
-                    Description = Brand.Description
-                };
-                listBrandViewModel.Add(objBrandViewModel);
-            }
-            return listBrandViewModel;
-        }
-        public async Task<bool> Update(BrandViewModel objBrandViewModel, string DeletedUser)
-        {
-            try
-            {
-                return false;
-                //Brand brand = new Brand
-                //{
-                //    ID = objBrandViewModel,
-                //    DefaultImage = objBrandViewModel.defaultImages,
-                //    Description = objBrandViewModel.defaultImages
-                //};
-                //this.unitOfWork.BrandRepository.Insert(brand);
-                //await this.unitOfWork.SaveChangeAsync();
-                //return true;
-            }
-            catch (Exception objEx)
-            {
-                ErrorLogs errorLogs = new ErrorLogs
-                {
-                    ErrorLog = objEx.ToString(),
-                    FunctionName = MethodBase.GetCurrentMethod().ToString(),
-                    ModuleName = "PM->Brand"
+    //        var listBrand=this.unitOfWork.BrandRepository.Get(filter:p=>p.isDeleted==false,number: intNumber,skippage:intSkippage);
+    //        var listBrandViewModel = new List<BrandViewModel>();
+    //        foreach (var Brand in listBrand)
+    //        {
+    //            BrandViewModel objBrandViewModel = new BrandViewModel
+    //            {
+    //                defaultImages = Brand.DefaultImage,
+    //                nameBrand = Brand.Name,
+    //                Description = Brand.Description
+    //            };
+    //            listBrandViewModel.Add(objBrandViewModel);
+    //        }
+    //        return listBrandViewModel;
+    //    }
+    //    public async Task<bool> Update(BrandViewModel objBrandViewModel, string DeletedUser)
+    //    {
+    //        try
+    //        {
+    //            return false;
+    //            //Brand brand = new Brand
+    //            //{
+    //            //    ID = objBrandViewModel,
+    //            //    DefaultImage = objBrandViewModel.defaultImages,
+    //            //    Description = objBrandViewModel.defaultImages
+    //            //};
+    //            //this.unitOfWork.BrandRepository.Insert(brand);
+    //            //await this.unitOfWork.SaveChangeAsync();
+    //            //return true;
+    //        }
+    //        catch (Exception objEx)
+    //        {
+    //            ErrorLogs errorLogs = new ErrorLogs
+    //            {
+    //                ErrorLog = objEx.ToString(),
+    //                FunctionName = MethodBase.GetCurrentMethod().ToString(),
+    //                ModuleName = "PM->Brand"
 
-                };
-                this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
-                await this.unitOfWork.SaveChangeAsync();
-                return false;
-            }
-        }
-    }
+    //            };
+    //            this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
+    //            await this.unitOfWork.SaveChangeAsync();
+    //            return false;
+    //        }
+    //    }
+    //}
 }
