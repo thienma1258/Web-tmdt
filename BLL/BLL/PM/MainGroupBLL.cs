799a1df18b0d0cf17e9b4c60a87c41fb44c838d1
Food <cpud1258@gmail.com>
2018-08-02 18:03:47 +0700

    them log

---
+++ BLL/BLL/PM/MainGroupBLL.cs
@@ -0,0 +1,108 @@
+﻿using DAL;
+using DAL.Model;
+using DAL.Model.PM;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BLL.PM
+{
+    public class MainGroupBLL
+    {
+        public IUnitOfWork unitOfWork;
+        public MainGroupBLL(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task<bool> Update(MainGroup MainGroup)
+        {
+            try
+            {
+                this.unitOfWork.MainGroupRepository.Update(MainGroup);
+                await this.unitOfWork.SaveChangeAsync();
+                return true;
+            }
+            catch (Exception objEx)
+            {
+                ErrorLogs errorLogs = new ErrorLogs
+                {
+                    ErrorLog = objEx.ToString(),
+                    FunctionName = MethodBase.GetCurrentMethod().ToString(),
+                    ModuleName = "PM->Brand"
+
+                };
+                this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
+                await this.unitOfWork.SaveChangeAsync();
+                return false;
+            }
+        }
+        public async Task<bool> Delete(MainGroup MainGroup)
+        {
+            try
+            {
+                this.unitOfWork.MainGroupRepository.Delete(MainGroup);
+                await this.unitOfWork.SaveChangeAsync();
+                return true;
+            }
+            catch (Exception objEx)
+            {
+                ErrorLogs errorLogs = new ErrorLogs
+                {
+                    ErrorLog = objEx.ToString(),
+                    FunctionName = MethodBase.GetCurrentMethod().ToString(),
+                    ModuleName = "PM->Brand"
+
+                };
+                this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
+                await this.unitOfWork.SaveChangeAsync();
+                return false;
+            }
+        }
+        public async Task<bool> Add(MainGroup MainGroup)
+        {
+            try
+            {
+                this.unitOfWork.MainGroupRepository.Insert(MainGroup);
+                await this.unitOfWork.SaveChangeAsync();
+                return true;
+            }
+            catch (Exception objEx)
+            {
+                ErrorLogs errorLogs = new ErrorLogs
+                {
+                    ErrorLog = objEx.ToString(),
+                    FunctionName = MethodBase.GetCurrentMethod().ToString(),
+                    ModuleName = "PM->Brand",
+                    TableName = "Brand"
+
+                };
+                this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
+                await this.unitOfWork.SaveChangeAsync();
+                return false;
+            }
+        }
+        public async Task<IEnumerable<MainGroup>> Get(int intNumber = -1, int intSkippage = -1)
+        {
+            try
+            {
+                return this.unitOfWork.MainGroupRepository.Get(filter: p => p.isDeleted == false, number: intNumber, skippage: intSkippage);
+
+            }
+            catch (Exception objEx)
+            {
+                ErrorLogs errorLogs = new ErrorLogs
+                {
+                    ErrorLog = objEx.ToString(),
+                    FunctionName = MethodBase.GetCurrentMethod().ToString(),
+                    ModuleName = "PM->MainGroup"
+
+                };
+                this.unitOfWork.ErrorLogsRepository.Insert(errorLogs);
+                await this.unitOfWork.SaveChangeAsync();
+                return null;
+            }
+        }
+    }
+}
