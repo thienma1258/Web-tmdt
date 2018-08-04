185a34ae91d6f644fbd88db4aa631464894ed87d
Food <cpud1258@gmail.com>
2018-08-04 17:28:40 +0700

    add file SaleOrderLogsRepository

---
+++ DAL/Repository/Log/ISaleOrderLogsRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Model.Log;
+using DAL.Model.SM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repository.Log
+{
+    public interface ISaleOrderLogsRepository:IGenericRepository<SaleOrderLogs,string>
+    {
+
+    }
+}
---
+++ DAL/Repository/Log/Implement/SaleOrderLogsRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.DataContext;
+using DAL.Model.Log;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repository.Log.Implement
+{
+    public class SaleOrderLogsRepository : GenericRepository<SaleOrderLogs, string>,ISaleOrderLogsRepository
+    {
+        public SaleOrderLogsRepository(ShopContext context) : base(context)
+        {
+        }
+    }
+}
