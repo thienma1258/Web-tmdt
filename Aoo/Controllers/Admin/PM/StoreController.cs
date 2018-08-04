cd0c6a34f91395886aef8d7ce13546253f50d135
sieunhan97 <ducnhan1551997@gmail.com>
2018-08-04 17:33:56 +0700

    Store 2

--- Aoo/Controllers/Admin/PM/StoreController.cs
+++ Aoo/Controllers/Admin/PM/StoreController.cs
@@ -59,13 +59,44 @@ namespace Aoo.Controllers.Admin.PM
             return View();
 
         }
-        public IActionResult EditStore()
+        public async Task<IActionResult> EditStore(string id)
+        {
+            Store objStore = await this.StoreBLL.Find(id);
+            ViewModels.PM.Store.EditViewStoreModel editViewStoreModel=new ViewModels.PM.Store.EditViewStoreModel
+            {
+                ID=objStore.ID,
+                NameStore=objStore.NameStore,
+                Description=objStore.Description,
+
+            };
+            return View(editViewStoreModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditStore(ViewModels.PM.Store.EditViewStoreModel editViewStoreModel)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                ImageErrorModel imageErrorModel;
+                MemoryStream memoryStream = new MemoryStream();
+                await editViewStoreModel.DefaultImage.CopyToAsync(memoryStream);
+                string ImagePath = this.ImageServices.UploadImage(memoryStream, editViewStoreModel.DefaultImage.FileName, out imageErrorModel);
+                if (imageErrorModel.isSuccess)
+                {
+                    Store objStore = await this.StoreBLL.Find(editViewStoreModel.ID);
+                    objStore.Description = editViewStoreModel.Description;
+                    objStore.NameStore = editViewStoreModel.NameStore;
+                    objStore.DefaultImage = ImagePath;
+                    await StoreBLL.Update(objStore);
+                }
+            }
+
+            return View();
+        }
+            public IActionResult DeleteStore()
         {
             return View();
         }
-        public IActionResult DeleteStore()
-        {
-            return View();
-        }
+       
     }
 }
---
+++ Aoo/ViewModels/PM/Store/EditViewStoreModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Aoo.ViewModels.PM.Store
+{
+    public class EditViewStoreModel
+    {
+        public string ID { get; set; }
+        [Required]
+        public string NameStore { get; set; }
+        [Required]
+        public IFormFile DefaultImage { get; set; }
+        [MaxLength(80)]
+        [MinLength(5)]
+        [Required]
+        [DisplayName("Mô tả")]
+        public string Description { get; set; }
+    }
+}
