3b03f51822a3ef92c0e1dd868c0e2e4775c15001
Food <cpud1258@gmail.com>
2018-08-07 14:44:10 +0700

    them 2 file ErrorLogsBLL

---
+++ BLL/BLL/Log/IErrorLogsBLL.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BLL.BLL.Log
+{
+    public interface IErrorLogsBLL
+    {
+        int Cout(Expression<Func<ErrorLogs, bool>> filter = null);
+
+    }
+}
---
+++ BLL/BLL/Log/Implement/ErrorLogsBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using DAL;
+using DAL.Model;
+
+namespace BLL.BLL.Log.Implement
+{
+    public class ErrorLogsBLL :GenericBLL, IErrorLogsBLL
+    {
+        public ErrorLogsBLL(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public int Cout(Expression<Func<ErrorLogs, bool>> filter = null)
+        {
+            return this.unitOfWork.ErrorLogsRepository.Cout(filter);
+        }
+    }
+}
